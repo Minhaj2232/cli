bdee98102dc46e317946c06055e97586e4d8b48f
Babak K. Shandiz <babakks@github.com>
2025-06-26 10:52:19 +0100

    fix(run view): use Actions API to retrieve job logs
    As a fallback mechanism, the Actions API is now used to retrieve job
    logs when the corresponding file entry is not found in the downloaded
    ZIP archive.
    
    Signed-off-by: Babak K. Shandiz <babakks@github.com>

--- pkg/cmd/run/view/view.go
+++ pkg/cmd/run/view/view.go
@@ -10,12 +10,10 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
-	"regexp"
+	"slices"
 	"sort"
 	"strconv"
-	"strings"
 	"time"
-	"unicode/utf16"
 
 	"github.com/MakeNowJust/heredoc"
 	"github.com/cli/cli/v2/api"
@@ -322,9 +320,12 @@ func runView(opts *ViewOptions) error {
 		}
 		defer runLogZip.Close()
 
-		attachRunLog(&runLogZip.Reader, jobs)
-
-		return displayRunLog(opts.IO.Out, jobs, opts.LogFailed)
+		return displayRunLog(
+			opts.IO.Out,
+			jobs,
+			getLogs(&runLogZip.Reader, httpClient, repo, jobs),
+			opts.LogFailed,
+		)
 	}
 
 	prNumber := ""
@@ -535,202 +536,48 @@ func promptForJob(prompter shared.Prompter, cs *iostreams.ColorScheme, jobs []sh
 	return nil, nil
 }
 
-const JOB_NAME_MAX_LENGTH = 90
-
-func getJobNameForLogFilename(name string) string {
-	// As described in https://github.com/cli/cli/issues/5011#issuecomment-1570713070, there are a number of steps
-	// the server can take when producing the downloaded zip file that can result in a mismatch between the job name
-	// and the filename in the zip including:
-	//  * Removing characters in the job name that aren't allowed in file paths
-	//  * Truncating names that are too long for zip files
-	//  * Adding collision deduplicating numbers for jobs with the same name
-	//
-	// We are hesitant to duplicate all the server logic due to the fragility but it may be unavoidable. Currently, we:
-	// * Strip `/` which occur when composite action job names are constructed of the form `<JOB_NAME`> / <ACTION_NAME>`
-	// * Truncate long job names
-	//
-	sanitizedJobName := strings.ReplaceAll(name, "/", "")
-	sanitizedJobName = strings.ReplaceAll(sanitizedJobName, ":", "")
-	sanitizedJobName = truncateAsUTF16(sanitizedJobName, JOB_NAME_MAX_LENGTH)
-	return sanitizedJobName
-}
-
-// A job run log file is a top-level .txt file whose name starts with an ordinal
-// number; e.g., "0_jobname.txt".
-func jobLogFilenameRegexp(job shared.Job) *regexp.Regexp {
-	sanitizedJobName := getJobNameForLogFilename(job.Name)
-	re := fmt.Sprintf(`^\d+_%s\.txt$`, regexp.QuoteMeta(sanitizedJobName))
-	return regexp.MustCompile(re)
-}
-
-// A legacy job run log file is a top-level .txt file whose name starts with a
-// negative number which is the ID of the run; e.g., "-2147483648_jobname.txt".
-func legacyJobLogFilenameRegexp(job shared.Job) *regexp.Regexp {
-	sanitizedJobName := getJobNameForLogFilename(job.Name)
-	re := fmt.Sprintf(`^-\d+_%s\.txt$`, regexp.QuoteMeta(sanitizedJobName))
-	return regexp.MustCompile(re)
-}
-
-func stepLogFilenameRegexp(job shared.Job, step shared.Step) *regexp.Regexp {
-	sanitizedJobName := getJobNameForLogFilename(job.Name)
-	re := fmt.Sprintf(`^%s\/%d_.*\.txt$`, regexp.QuoteMeta(sanitizedJobName), step.Number)
-	return regexp.MustCompile(re)
-}
-
-/*
-If you're reading this comment by necessity, I'm sorry and if you're reading it for fun, you're welcome, you weirdo.
-
-What is the length of this string "a😅😅"? If you said 9 you'd be right. If you said 3 or 5 you might also be right!
-
-Here's a summary:
-
-	"a" takes 1 byte (`\x61`)
-	"😅" takes 4 `bytes` (`\xF0\x9F\x98\x85`)
-	"a😅😅" therefore takes 9 `bytes`
-	In Go `len("a😅😅")` is 9 because the `len` builtin counts `bytes`
-	In Go `len([]rune("a😅😅"))` is 3 because each `rune` is 4 `bytes` so each character fits within a `rune`
-	In C# `"a😅😅".Length` is 5 because `.Length` counts `Char` objects, `Chars` hold 2 bytes, and "😅" takes 2 Chars.
-
-But wait, what does C# have to do with anything? Well the server is running C#. Which server? The one that serves log
-files to us in `.zip` format of course! When the server is constructing the zip file to avoid running afoul of a 260
-byte zip file path length limitation, it applies transformations to various strings in order to limit their length.
-In C#, the server truncates strings with this function:
-
-	public static string TruncateAfter(string str, int max)
-	{
-		string result = str.Length > max ? str.Substring(0, max) : str;
-		result = result.Trim();
-		return result;
-	}
-
-This seems like it would be easy enough to replicate in Go but as we already discovered, the length of a string isn't
-as obvious as it might seem. Since C# uses UTF-16 encoding for strings, and Go uses UTF-8 encoding and represents
-characters by runes (which are an alias of int32) we cannot simply slice the string without any further consideration.
-Instead, we need to encode the string as UTF-16 bytes, slice it and then decode it back to UTF-8.
-
-Interestingly, in C# length and substring both act on the Char type so it's possible to slice into the middle of
-a visual, "representable" character. For example we know `"a😅😅".Length` = 5 (1+2+2) and therefore Substring(0,4)
-results in the final character being cleaved in two, resulting in "a😅�". Since our int32 runes are being encoded as
-2 uint16 elements, we also mimic this behaviour by slicing into the UTF-16 encoded string.
-
-Here's a program you can put into a dotnet playground to see how C# works:
-
-	using System;
-	public class Program {
-	  public static void Main() {
-	    string s = "a😅😅";
-	    Console.WriteLine("{0} {1}", s.Length, s);
-	    string t = TruncateAfter(s, 4);
-	    Console.WriteLine("{0} {1}", t.Length, t);
-	  }
-	  public static string TruncateAfter(string str, int max) {
-	    string result = str.Length > max ? str.Substring(0, max) : str;
-	    return result.Trim();
-	  }
-	}
-
-This will output:
-5 a😅😅
-4 a😅�
-*/
-func truncateAsUTF16(str string, max int) string {
-	// Encode the string to UTF-16 to count code units
-	utf16Encoded := utf16.Encode([]rune(str))
-	if len(utf16Encoded) > max {
-		// Decode back to UTF-8 up to the max length
-		str = string(utf16.Decode(utf16Encoded[:max]))
-	}
-	return strings.TrimSpace(str)
-}
-
-// This function takes a zip file of logs and a list of jobs.
-// Structure of zip file
-//
-//	zip/
-//	├── jobname1/
-//	│   ├── 1_stepname.txt
-//	│   ├── 2_anotherstepname.txt
-//	│   ├── 3_stepstepname.txt
-//	│   └── 4_laststepname.txt
-//	├── jobname2/
-//	|   ├── 1_stepname.txt
-//	|   └── 2_somestepname.txt
-//	├── 0_jobname1.txt
-//	├── 1_jobname2.txt
-//	└── -9999999999_jobname3.txt
-//
-// It iterates through the list of jobs and tries to find the matching
-// log in the zip file. If the matching log is found it is attached
-// to the job.
-//
-// The top-level .txt files include the logs for an entire job run. Note that
-// the prefixed number is either:
-//   - An ordinal and cannot be mapped to the corresponding job's ID.
-//   - A negative integer which is the ID of the job in the old Actions service.
-//     The service right now tries to get logs and use an ordinal in a loop.
-//     However, if it doesn't get the logs, it falls back to an old service
-//     where the ID can apparently be negative.
-func attachRunLog(rlz *zip.Reader, jobs []shared.Job) {
-	for i, job := range jobs {
-		// As a highest priority, we try to use the step logs first. We have seen zips that surprisingly contain
-		// step logs, normal job logs and legacy job logs. In this case, both job logs would be ignored. We have
-		// never seen a zip containing both job logs and no step logs, however, it may be possible. In that case
-		// let's prioritise the normal log over the legacy one.
-		jobLog := matchFileInZIPArchive(rlz, jobLogFilenameRegexp(job))
-		if jobLog == nil {
-			jobLog = matchFileInZIPArchive(rlz, legacyJobLogFilenameRegexp(job))
-		}
-		jobs[i].Log = jobLog
-
-		for j, step := range job.Steps {
-			jobs[i].Steps[j].Log = matchFileInZIPArchive(rlz, stepLogFilenameRegexp(job, step))
-		}
-	}
-}
-
-func matchFileInZIPArchive(zr *zip.Reader, re *regexp.Regexp) *zip.File {
-	for _, file := range zr.File {
-		if re.MatchString(file.Name) {
-			return file
-		}
-	}
-	return nil
-}
-
-func displayRunLog(w io.Writer, jobs []shared.Job, failed bool) error {
+func displayRunLog(w io.Writer, jobs []shared.Job, logs logs, onlyFailedLogs bool) error {
 	for _, job := range jobs {
+		if onlyFailedLogs && !shared.IsFailureState(job.Conclusion) {
+			continue
+		}
+
 		// To display a run log, we first try to compile it from individual step
 		// logs, because this way we can prepend lines with the corresponding
 		// step name. However, at the time of writing, logs are sometimes being
 		// served by a service that doesn’t include the step logs (none of them),
 		// in which case we fall back to print the entire job run log.
-		var hasStepLogs bool
+		hasStepLogs := slices.ContainsFunc(job.Steps, func(step shared.Step) bool {
+			// if any step has a log, then we consider that as having all step logs.
+			return logs.hasStep(job.ID, step.Number)
+		})
 
-		steps := job.Steps
-		sort.Sort(steps)
-		for _, step := range steps {
-			if failed && !shared.IsFailureState(step.Conclusion) {
-				continue
-			}
-			if step.Log == nil {
-				continue
-			}
-			hasStepLogs = true
-			prefix := fmt.Sprintf("%s\t%s\t", job.Name, step.Name)
-			if err := printZIPFile(w, step.Log, prefix); err != nil {
-				return err
-			}
-		}
-
+		// If we have step logs then we are going to print them.
 		if hasStepLogs {
-			continue
-		}
+			steps := job.Steps
+			sort.Sort(steps)
+			for _, step := range steps {
+				if !logs.hasStep(job.ID, step.Number) {
+					continue
+				}
+				if onlyFailedLogs && !shared.IsFailureState(step.Conclusion) {
+					continue
+				}
 
-		if failed && !shared.IsFailureState(job.Conclusion) {
-			continue
-		}
+				logReader, err := logs.forStep(job.ID, step.Number)
+				if err != nil {
+					return err
+				}
+				defer logReader.Close()
 
-		if job.Log == nil {
+				prefix := fmt.Sprintf("%s\t%s\t", job.Name, step.Name)
+				if err := copyLogWithLinePrefix(w, logReader, prefix); err != nil {
+					return err
+				}
+			}
+
+			// We've gone through all the steps and printed their logs,
+			// so let's move onto the next job.
 			continue
 		}
 
@@ -739,8 +586,20 @@ func displayRunLog(w io.Writer, jobs []shared.Job, failed bool) error {
 		// formatting to avoid breaking any code or script that rely on the
 		// tab-delimited formatting. So, an unknown-step placeholder is used
 		// instead of the actual step name.
+
+		// Note that, when calling GetLog, it is possible that it will fetch the
+		// log from the API (because it's not already there in the ZIP file). And
+		// in that case, we're sending API requests sequentially and waiting for
+		// them to complete before proceeding to the next job. This can be
+		// improved by doing them concurrently.
+		logReader, err := logs.forJob(job.ID)
+		if err != nil {
+			return err
+		}
+		defer logReader.Close()
+
 		prefix := fmt.Sprintf("%s\tUNKNOWN STEP\t", job.Name)
-		if err := printZIPFile(w, job.Log, prefix); err != nil {
+		if err := copyLogWithLinePrefix(w, logReader, prefix); err != nil {
 			return err
 		}
 	}
@@ -748,14 +607,8 @@ func displayRunLog(w io.Writer, jobs []shared.Job, failed bool) error {
 	return nil
 }
 
-func printZIPFile(w io.Writer, file *zip.File, prefix string) error {
-	f, err := file.Open()
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
+func copyLogWithLinePrefix(w io.Writer, r io.Reader, prefix string) error {
+	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		fmt.Fprintf(w, "%s%s\n", prefix, scanner.Text())
 	}
